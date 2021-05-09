3657ba571fbde244a0b3eea8934014a6a100d0a2
drsmile <drsmile.1001@gmail.com>
2021-05-09 20:36:33 +0800

    feat: counter 頁獨立檔案，實作 layout 教學

--- lib/main.dart
+++ lib/main.dart
@@ -1,56 +1,7 @@
 import 'package:flutter/material.dart';
 
-class CounterDisplay extends StatelessWidget {
-  CounterDisplay({required this.count});
-
-  final int count;
-
-  @override
-  Widget build(BuildContext context) {
-    return Text('Count: $count');
-  }
-}
-
-class CounterIncrementor extends StatelessWidget {
-  CounterIncrementor({required this.onPressed});
-
-  final VoidCallback onPressed;
-
-  @override
-  Widget build(BuildContext context) {
-    return ElevatedButton(
-      onPressed: onPressed,
-      child: Text('Increment'),
-    );
-  }
-}
-
-class Counter extends StatefulWidget {
-  @override
-  _CounterState createState() => _CounterState();
-}
-
-class _CounterState extends State<Counter> {
-  int _counter = 0;
-
-  void _increment() {
-    setState(() {
-      ++_counter;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Row(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: <Widget>[
-        CounterIncrementor(onPressed: _increment),
-        SizedBox(width: 16),
-        CounterDisplay(count: _counter),
-      ],
-    );
-  }
-}
+import 'counter.dart';
+import 'layout-tutorial.dart';
 
 class MyApp extends StatefulWidget {
   @override
@@ -69,17 +20,18 @@ class _MyAppState extends State<MyApp> {
             MaterialPage(
                 key: ValueKey('ListPage'),
                 child: MenuScreen(
-                  pageNames: ["aaa", "bbb"],
+                  pageNames: ['Counter', 'LayoutPractice'],
                   onTapped: (page) {
                     setState(() {
                       _selectedPage = page;
                     });
                   },
                 )),
-            if (_selectedPage != null)
+            if (_selectedPage == 'LayoutPractice')
               MaterialPage(
-                  key: ValueKey(_selectedPage),
-                  child: Scaffold(appBar: AppBar(), body: Text(_selectedPage!)))
+                  key: ValueKey(_selectedPage), child: LayoutPractice())
+            else if (_selectedPage == 'Counter')
+              MaterialPage(key: ValueKey(_selectedPage), child: Counter())
           ],
           onPopPage: (route, result) {
             if (!route.didPop(result)) return false;
